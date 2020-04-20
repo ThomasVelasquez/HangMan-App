eccde353459cbf79d5f1f08b0367e1c6b7e1f850
Omar Cardenas <60041964+omarcv07@users.noreply.github.com>
2020-04-20 15:35:07 -0400

    Update requests.js

--- requests.js
+++ requests.js
@@ -8,29 +8,3 @@ const getPuzzle = async (wordCount) => {
         throw new Error('Unable to get puzzle')
     }
 }
-
-const getCurrentCountry = async () => {
-    const location = await getLocation()
-    return getCountry(location.country)
-}
-
-const getCountry = async (countryCode) => {    
-    const response = await fetch('https://restcountries.eu/rest/v2/all')
-        
-        if (response.status === 200) {
-            const data = await response.json()
-            return data.find((country) => country.alpha2Code === countryCode)
-        } else {
-            throw new Error('Unable to fetch data')
-        }
-}
-
-const getLocation = async () => {
-    const response = await fetch('http://ipinfo.io/json?token=5837ba9ca07203')
-        if (response.status === 200) {
-            return await response.json()
-        } else {
-            throw new Error('Could not find your Location')
-        }
-}
-
